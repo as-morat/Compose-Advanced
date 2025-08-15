8dcef66b8e5f5f7804b20df7554111869b1809ee
as-morat <morat9511@gmail.com>
2025-08-15 22:59:06 +0600

    Demonstrate recomposition concept in Compose

---
+++ app/src/main/java/com/example/composeadvanced/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.composeadvanced
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.composeadvanced.ui.theme.ComposeAdvancedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeAdvancedTheme {
+                Recomposition()
+            }
+        }
+    }
+}
